08220cb7ef0b8727748edaf8ac6f5283bd8aff51
Alex Pariseau <arpariseau@gmail.com>
2020-05-29 09:47:03 -0600

    Dish can calculate total calories

--- spec/features/dishes/dish_show_spec.rb
+++ spec/features/dishes/dish_show_spec.rb
@@ -1,32 +1,36 @@
 require 'rails_helper'
 
 describe 'Dish show page' do
-  it "can show a dish's ingredients" do
-    swede = Chef.create(name: "Bjorn, the Swedish Chef")
-    pasta = Dish.create(name: "Spaghetti and Meatballs",
+  before :each do
+    @swede = Chef.create(name: "Bjorn, the Swedish Chef")
+    @pasta = Dish.create(name: "Spaghetti and Meatballs",
                         description: "Pasta noodles with bits of meat",
-                        chef_id: swede.id)
-    spaghetti = Ingredient.create(name: "spaghetti",
+                        chef_id: @swede.id)
+    @spaghetti = Ingredient.create(name: "spaghetti",
                                   calories: 200)
-    sauce = Ingredient.create(name: "tomato sauce",
+    @sauce = Ingredient.create(name: "tomato sauce",
                               calories: 150)
-    meatballs = Ingredient.create(name: "meatballs",
+    @meatballs = Ingredient.create(name: "meatballs",
                                   calories: 250)
-    DishIngredient.create(dish_id: pasta.id, ingredient_id: spaghetti.id)
-    DishIngredient.create(dish_id: pasta.id, ingredient_id: sauce.id)
-    DishIngredient.create(dish_id: pasta.id, ingredient_id: meatballs.id)
+    DishIngredient.create(dish_id: @pasta.id, ingredient_id: @spaghetti.id)
+    DishIngredient.create(dish_id: @pasta.id, ingredient_id: @sauce.id)
+    DishIngredient.create(dish_id: @pasta.id, ingredient_id: @meatballs.id)
 
-    visit "/dishes/#{pasta.id}"
-
-    expect(page).to have_content(swede.name)
-    expect(page).to have_content(spaghetti.name)
-    expect(page).to have_content(sauce.name)
-    expect(page).to have_content(meatballs.name)
+    visit "/dishes/#{@pasta.id}"
+  end
+  it "can show ingredients of a dish" do
+    expect(page).to have_content(@swede.name)
+    expect(page).to have_content(@spaghetti.name)
+    expect(page).to have_content(@sauce.name)
+    expect(page).to have_content(@meatballs.name)
+  end
+  it "can show a calorie count of a dish" do
+    calorie_count = @spaghetti.calories + @sauce.calories + @meatballs.calories
+    expect(page).to have_content("Total Calorie Count: #{calorie_count}")
   end
 end
 
-# Story 1 of 3
+# Story 2 of 3
 # As a visitor
 # When I visit a dish's show page
-# I see a list of ingredients for that dish
-# and the chef's name
+# I see the total calorie count for that dish.
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+  describe "methods" do
+    it '#total_calories' do
+      swede = Chef.create(name: "Bjorn, the Swedish Chef")
+      pasta = Dish.create(name: "Spaghetti and Meatballs",
+                          description: "Pasta noodles with bits of meat",
+                          chef_id: swede.id)
+      spaghetti = Ingredient.create(name: "spaghetti",
+                                    calories: 200)
+      sauce = Ingredient.create(name: "tomato sauce",
+                                calories: 150)
+      meatballs = Ingredient.create(name: "meatballs",
+                                    calories: 250)
+      DishIngredient.create(dish_id: pasta.id, ingredient_id: spaghetti.id)
+      DishIngredient.create(dish_id: pasta.id, ingredient_id: sauce.id)
+      DishIngredient.create(dish_id: pasta.id, ingredient_id: meatballs.id)
+
+      calorie_count = spaghetti.calories + sauce.calories + meatballs.calories
+      expect(pasta.total_calories).to eq(calorie_count)
+    end
+  end
 end
